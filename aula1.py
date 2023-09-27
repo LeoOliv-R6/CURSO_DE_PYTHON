d63e75c6bab97363ffe9f23b3bdf3b7c13258be7
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-09-27 16:27:12 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# Permite escrever um comentário (# é usado pra comentar)
+
+print("Tomar agua com gas é extremamento bom, mas pagar mais de 2 reais é embasado!")
+
+'''
+DocString
+Não é um comentário!
+Usado para fazer anotações anotação
+'''
+"""
+Ou esse tipo
+"""
---
+++ aula2.py
@@ -0,0 +1,5 @@
+
+print('A funcao print recebe um argumento')
+
+print(12, 34, sep=' separador, tanto faz o tipo de aspas, ele separa as palavras ')
+print(5, 4, sep=" - ")
