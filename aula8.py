6f41d88911d7f1a57a4629ddca26d0d88ffab7b5
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-09-28 14:14:08 -0300

    refinando o conhecimento

---
+++ aula8.py
@@ -0,0 +1,15 @@
+import datetime
+
+nome = 'Leonardo'
+sobrenome = 'Oliveira'
+idade = 21
+anoNascimento = 2023 - idade
+maiorDeIdade = idade >= 18
+alturaMetros = (1.80)
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', anoNascimento)
+print('É maior de idade?', maiorDeIdade)
+print('Altura em metros:', alturaMetros)
